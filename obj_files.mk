97774a59415aa6ca700da8092612f43fd491e92e
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-09-28 12:16:01 -0400

    fixed the blasted spacing

--- obj_files.mk
+++ obj_files.mk
@@ -1,40 +1,40 @@
 # Linker order for every file, passed to the Metrowerks linker.
 
-INIT_O_FILES := 						            \
+INIT_O_FILES :=									\
 	$(BUILD_DIR)/asm/init.o							\
 
-EXTAB_O_FILES :=                                    \
+EXTAB_O_FILES :=								\
 	$(BUILD_DIR)/asm/extab.o						\
 
-EXTABINDEX_O_FILES :=                               \
+EXTABINDEX_O_FILES :=								\
 	$(BUILD_DIR)/asm/exidx.o						\
 
-TEXT_O_FILES := 						            \
+TEXT_O_FILES :=									\
 	$(BUILD_DIR)/asm/text.o							\
 
-CTORS_O_FILES :=                                    \
+CTORS_O_FILES :=								\
 	$(BUILD_DIR)/asm/ctors.o						\
 
-DTORS_O_FILES :=                                    \
+DTORS_O_FILES :=								\
 	$(BUILD_DIR)/asm/dtors.o						\
 
-RODATA_O_FILES :=                                   \
+RODATA_O_FILES :=								\
 	$(BUILD_DIR)/asm/rodata.o						\
 
-DATA_O_FILES :=                                     \
+DATA_O_FILES :=									\
 	$(BUILD_DIR)/asm/data.o							\
 
-BSS_O_FILES :=                                      \
+BSS_O_FILES :=									\
 	$(BUILD_DIR)/asm/bss.o							\
 
-SDATA_O_FILES :=                                    \
+SDATA_O_FILES :=								\
 	$(BUILD_DIR)/asm/sdata.o						\
 
-SBSS_O_FILES :=										\
+SBSS_O_FILES :=									\
 	$(BUILD_DIR)/asm/sbss.o							\
 
-SDATA2_O_FILES :=                                   \
+SDATA2_O_FILES :=								\
 	$(BUILD_DIR)/asm/sdata2.o						\
 	
-SBSS2_O_FILES :=									\
+SBSS2_O_FILES :=								\
 #	$(BUILD_DIR)/asm/sbss2.o						\
