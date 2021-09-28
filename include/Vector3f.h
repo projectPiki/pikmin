b495ce709587dc34d92d5b0e01d30b2bbddb8926
intns <84647527+intns@users.noreply.github.com>
2021-09-28 18:22:37 +0100

    Seperate into .h and .cpp file

---
+++ include/Vector3f.h
@@ -0,0 +1,7 @@
+#include "types.h"
+
+class Vector3f {
+	f32 x, y, z;
+	
+	Vector3f(float const& x, float const& y, float const& z);
+};
--- src/Vector3f.cpp
+++ src/Vector3f.cpp
@@ -1,7 +1,4 @@
-class Vector3f {
-	float x, y, z;
-	Vector3f(float const& x, float const& y, float const& z);
-};
+#include "Vector3f.h"
 
 Vector3f::Vector3f(float const&_x, float const&_y, float const&_z) :
 	x(_x), y(_y), z(_z ) {
