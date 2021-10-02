530daf5fb2d31ce8f43602efe4bb81931ceb9489
intns <84647527+intns@users.noreply.github.com>
2021-10-02 17:14:30 +0100

    remove deps for sdata2

--- asm/sdata2.s
+++ asm/sdata2.s
@@ -2,154 +2,177 @@
 .section .sdata2, "a"  # 0x803E8200 - 0x803EC840
 .global lbl_803E8200
 lbl_803E8200:
-	.incbin "baserom.dol", 0x2F1140, 0x8
+	.4byte 0x4453504D
+	.4byte 0x49580000
 .global lbl_803E8208
 lbl_803E8208:
-	.incbin "baserom.dol", 0x2F1148, 0x8
+	.4byte 0x4D495849
+	.4byte 0x4E470000
 .global lbl_803E8210
 lbl_803E8210:
-	.incbin "baserom.dol", 0x2F1150, 0x8
+	.4byte 0x5346525F
+	.4byte 0x44535000
 .global $$232
 $$232:
-	.incbin "baserom.dol", 0x2F1158, 0x8
+	.4byte 0x43300000
+	.4byte 0x80000000
 .global lbl_803E8220
 lbl_803E8220:
-	.incbin "baserom.dol", 0x2F1160, 0x8
+	.4byte 0x00000000
+	.4byte 0x00000000
 .global lbl_803E8228
 lbl_803E8228:
-	.incbin "baserom.dol", 0x2F1168, 0x8
+	.4byte 0x6D786963
+	.4byte 0x2E630000
 .global lbl_803E8230
 lbl_803E8230:
-	.incbin "baserom.dol", 0x2F1170, 0x8
+	.4byte 0x45800000
+	.4byte 0x00000000
 .global lbl_803E8238
 lbl_803E8238:
-	.incbin "baserom.dol", 0x2F1178, 0x4
+	.4byte 0x46800000
 .global lbl_803E823C
 lbl_803E823C:
-	.incbin "baserom.dol", 0x2F117C, 0x4
+	.4byte 0x00000000
 .global lbl_803E8240
 lbl_803E8240:
-	.incbin "baserom.dol", 0x2F1180, 0x4
+	.4byte 0x3F800000
 .global lbl_803E8244
 lbl_803E8244:
-	.incbin "baserom.dol", 0x2F1184, 0x4
+	.4byte 0x3F000000
 .global lbl_803E8248
 lbl_803E8248:
-	.incbin "baserom.dol", 0x2F1188, 0x8
+	.4byte 0x45800000
+	.4byte 0x00000000
 .global $$2346
 $$2346:
-	.incbin "baserom.dol", 0x2F1190, 0x8
+	.4byte 0x43300000
+	.4byte 0x00000000
 .global lbl_803E8258
 lbl_803E8258:
-	.incbin "baserom.dol", 0x2F1198, 0x8
+	.4byte 0x3F8CCCCD
+	.4byte 0x00000000
 .global $$2264
 $$2264:
-	.incbin "baserom.dol", 0x2F11A0, 0x8
+	.4byte 0x43300000
+	.4byte 0x00000000
 .global lbl_803E8268
 lbl_803E8268:
-	.incbin "baserom.dol", 0x2F11A8, 0x4
+	.4byte 0x3F800000
 .global lbl_803E826C
 lbl_803E826C:
-	.incbin "baserom.dol", 0x2F11AC, 0x4
+	.4byte 0x42FE0000
 .global $$2125
 $$2125:
-	.incbin "baserom.dol", 0x2F11B0, 0x8
+	.4byte 0x43300000
+	.4byte 0x00000000
 .global $$2127
 $$2127:
-	.incbin "baserom.dol", 0x2F11B8, 0x8
+	.4byte 0x43300000
+	.4byte 0x80000000
 .global lbl_803E8280
 lbl_803E8280:
-	.incbin "baserom.dol", 0x2F11C0, 0x4
+	.4byte 0x00000000
 .global lbl_803E8284
 lbl_803E8284:
-	.incbin "baserom.dol", 0x2F11C4, 0x4
+	.4byte 0x42A00000
 .global lbl_803E8288
 lbl_803E8288:
-	.incbin "baserom.dol", 0x2F11C8, 0x4
+	.4byte 0x44160000
 .global lbl_803E828C
 lbl_803E828C:
-	.incbin "baserom.dol", 0x2F11CC, 0x4
+	.4byte 0x47000000
 .global lbl_803E8290
 lbl_803E8290:
-	.incbin "baserom.dol", 0x2F11D0, 0x8
+	.4byte 0x4B7FFFFF
+	.4byte 0x00000000
 .global $$258
 $$258:
-	.incbin "baserom.dol", 0x2F11D8, 0x8
+	.4byte 0x43300000
+	.4byte 0x80000000
 .global lbl_803E82A0
 lbl_803E82A0:
-	.incbin "baserom.dol", 0x2F11E0, 0x8
+	.4byte 0x00000000
+	.4byte 0x00000000
 .global $$2110
 $$2110:
-	.incbin "baserom.dol", 0x2F11E8, 0x8
+	.4byte 0x3FE00000
+	.4byte 0x00000000
 .global $$2111
 $$2111:
-	.incbin "baserom.dol", 0x2F11F0, 0x8
+	.4byte 0x40080000
+	.4byte 0x00000000
 .global lbl_803E82B8
 lbl_803E82B8:
-	.incbin "baserom.dol", 0x2F11F8, 0x4
+	.4byte 0x3FC90FDB
 .global lbl_803E82BC
 lbl_803E82BC:
-	.incbin "baserom.dol", 0x2F11FC, 0x4
+	.4byte 0x3B800000
 .global $$2124
 $$2124:
-	.incbin "baserom.dol", 0x2F1200, 0x8
+	.4byte 0x43300000
+	.4byte 0x00000000
 .global lbl_803E82C8
 lbl_803E82C8:
-	.incbin "baserom.dol", 0x2F1208, 0x8
+	.4byte 0x43800000
+	.4byte 0x00000000
 .global lbl_803E82D0
 lbl_803E82D0:
-	.incbin "baserom.dol", 0x2F1210, 0x8
+	.4byte 0x2F000000
+	.4byte 0x00000000
 .global lbl_803E82D8
 lbl_803E82D8:
-	.incbin "baserom.dol", 0x2F1218, 0x4
+	.4byte 0x42C80000
 .global lbl_803E82DC
 lbl_803E82DC:
-	.incbin "baserom.dol", 0x2F121C, 0x4
+	.4byte 0x42F00000
 .global $$2119
 $$2119:
-	.incbin "baserom.dol", 0x2F1220, 0x8
+	.4byte 0x43300000
+	.4byte 0x80000000
 .global $$2121
 $$2121:
-	.incbin "baserom.dol", 0x2F1228, 0x8
+	.4byte 0x43300000
+	.4byte 0x00000000
 .global lbl_803E82F0
 lbl_803E82F0:
-	.incbin "baserom.dol", 0x2F1230, 0x4
+	.4byte 0x47000000
 .global lbl_803E82F4
 lbl_803E82F4:
-	.incbin "baserom.dol", 0x2F1234, 0x4
+	.4byte 0x00000000
 .global lbl_803E82F8
 lbl_803E82F8:
-	.incbin "baserom.dol", 0x2F1238, 0x4
+	.4byte 0x3F800000
 .global lbl_803E82FC
 lbl_803E82FC:
-	.incbin "baserom.dol", 0x2F123C, 0x4
+	.4byte 0x46FFFE00
 .global lbl_803E8300
 lbl_803E8300:
-	.incbin "baserom.dol", 0x2F1240, 0x4
+	.4byte 0x43000000
 .global lbl_803E8304
 lbl_803E8304:
-	.incbin "baserom.dol", 0x2F1244, 0x4
+	.4byte 0x42700000
 .global lbl_803E8308
 lbl_803E8308:
-	.incbin "baserom.dol", 0x2F1248, 0x4
+	.4byte 0x42A00000
 .global lbl_803E830C
 lbl_803E830C:
-	.incbin "baserom.dol", 0x2F124C, 0x4
+	.4byte 0x43A3D5C3
 .global lbl_803E8310
 lbl_803E8310:
-	.incbin "baserom.dol", 0x2F1250, 0x4
+	.4byte 0x00000001
 .global lbl_803E8314
 lbl_803E8314:
-	.incbin "baserom.dol", 0x2F1254, 0x2
+	.2byte 0x0102
 .global lbl_803E8316
 lbl_803E8316:
-	.incbin "baserom.dol", 0x2F1256, 0x2
+	.2byte 0x0200
 .global lbl_803E8318
 lbl_803E8318:
-	.incbin "baserom.dol", 0x2F1258, 0x4
+	.4byte 0x00010200
 .global lbl_803E831C
 lbl_803E831C:
-	.incbin "baserom.dol", 0x2F125C, 0x2
+	.2byte 0x0200
 .global lbl_803E831E
 lbl_803E831E:
-	.incbin "baserom.dol", 0x2F125E, 0x2
+	.2byte 0x0200
