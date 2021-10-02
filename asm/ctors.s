bc99824ca27b1dc4b922a8841b96ebef8f1a14c9
intns <84647527+intns@users.noreply.github.com>
2021-10-02 16:04:51 +0100

    remove deps for ctors, dtors and exidx

--- asm/ctors.s
+++ asm/ctors.s
@@ -2,4 +2,27 @@
 .section .ctors, "wa"  # 0x80221F60 - 0x80221FC0
 .global __init_cpp_exceptions_reference
 __init_cpp_exceptions_reference:
-	.incbin "baserom.dol", 0x21EF60, 0x60
+	.4byte 0x802154FC
+	.4byte 0x80035A9C
+	.4byte 0x8003EE30
+	.4byte 0x80046EDC
+	.4byte 0x8004C8DC
+	.4byte 0x80052EC4
+	.4byte 0x80053B6C
+	.4byte 0x8005D0E4
+	.4byte 0x800697A4
+	.4byte 0x8008A028
+	.4byte 0x800C6524
+	.4byte 0x800CD744
+	.4byte 0x80112628
+	.4byte 0x801A5384
+	.4byte 0x801A7DD0
+	.4byte 0x801D59C8
+	.4byte 0x8021BE8C
+	.4byte 0x00000000
+	.4byte 0x00000000
+	.4byte 0x00000000
+	.4byte 0x00000000
+	.4byte 0x00000000
+	.4byte 0x00000000
+	.4byte 0x00000000
