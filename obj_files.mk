13ef5f7750001d85210ce529f4c235eb2b1e42ec
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-11-05 18:33:30 -0400

    match and link mutex_TRK
    more decomp and linking/splitting

--- obj_files.mk
+++ obj_files.mk
@@ -66,6 +66,9 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/src/gx/GXStubs.o\
 	$(BUILD_DIR)/asm/text_802140E8.o\
 	$(BUILD_DIR)/asm/text_8021C4CC.o\
+	$(BUILD_DIR)/asm/TRK_MINNOW_DOLPHIN/msghndlr.o\
+	$(BUILD_DIR)/asm/TRK_MINNOW_DOLPHIN/support.o\
+	$(BUILD_DIR)/src/TRK_MINNOW_DOLPHIN/mutex_TRK.o\
 	$(BUILD_DIR)/asm/TRK_MINNOW_DOLPHIN/notify.o\
 	$(BUILD_DIR)/asm/TRK_MINNOW_DOLPHIN/flush_cache.o\
 	$(BUILD_DIR)/asm/TRK_MINNOW_DOLPHIN/mem_TRK.o\
