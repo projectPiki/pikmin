ee2b53cf558f54d420803803454d5c05a7be29cb
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-11-16 00:46:23 -0500

    tentative nonmatching decomp of targcont.c

--- src/TRK_MINNOW_DOLPHIN/targcont.c
+++ src/TRK_MINNOW_DOLPHIN/targcont.c
@@ -1,26 +1,16 @@
-
+#include "dolphin/trk.h"
 
 /*
  * --INFO--
  * Address:	802207B8
  * Size:	000034
  */
-void TRKTargetContinue(void)
+
+unsigned int TRKTargetContinue(void)
 {
-/*
-.loc_0x0:
-  mflr      r0
-  li        r3, 0
-  stw       r0, 0x4(r1)
-  stwu      r1, -0x8(r1)
-  bl        -0xCC0
-  bl        -0x68
-  bl        -0x12CC
-  bl        -0xA0
-  li        r3, 0
-  addi      r1, r1, 0x8
-  lwz       r0, 0x4(r1)
-  mtlr      r0
-  blr
-*/
+	TRKTargetSetStopped(0);
+	UnreserveEXI2Port();
+	TRKSwapAndGo();
+	ReserveEXI2Port();
+	return 0;
 }
