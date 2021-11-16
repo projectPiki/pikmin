e34cf3ecc7b00129c25b51b0ee4c3c363bf77fc2
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-11-15 22:22:04 -0500

    organize and label TAIbeatle and TAIeffectAttack

--- obj_files.mk
+++ obj_files.mk
@@ -43,6 +43,8 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/plugPikiNishimura/SpiderAi.o\
 	$(BUILD_DIR)/asm/plugPikiNishimura/SpiderLeg.o\
 	$(BUILD_DIR)/asm/text_Nishimura.o\
+	$(BUILD_DIR)/asm/plugPikiYamashita/TAIeffectAttack.o\
+	$(BUILD_DIR)/asm/plugPikiYamashita/TAIbeatle.o\
 	$(BUILD_DIR)/asm/plugPikiYamashita/menuPanelMgr.o\
 	$(BUILD_DIR)/asm/plugPikiYamashita/TAIkabekuiA.o\
 	$(BUILD_DIR)/asm/plugPikiYamashita/TAIkabekuiB.o\
