22683ba6c1b5893e344d3ee34615ca21ed9cbde6
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-11-17 17:44:07 -0500

    decompile TAIanimation.cpp
    not linked due to EPPC4 scheduling, but matches otherwise
    
    epilogue is unscheduled while the main body needs scheduling on
    
    Co-Authored-By: SodiumChlorideLogic <19625889+SodiumChlorideLogic@users.noreply.github.com>

---
+++ include/TAIanimation.h
@@ -0,0 +1,23 @@
+#ifndef _TAIANIMATION_H
+#define _TAIANIMATION_H
+typedef class TAIanimation;
+typedef class AnimMgr;
+typedef class AnimInfo;
+
+class AnimInfo {
+	AnimInfo();
+};
+class AnimMgr {
+	AnimMgr();
+public:
+	AnimInfo* addAnimation(char*, bool);
+};
+
+class TAIanimation {
+	unsigned int _00; // _00
+	AnimMgr* m_animmgr; // _04
+	AnimInfo* m_animinfo; // _08
+
+	AnimInfo* addAnimation(char*);
+};
+#endif
