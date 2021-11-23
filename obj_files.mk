32bc5aa82bf1664e18e7bb6d4f5707988b470c86
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-11-22 21:07:25 -0500

    split and label ogMessage.s

--- obj_files.mk
+++ obj_files.mk
@@ -43,6 +43,7 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/plugPikiNishimura/SpiderAi.o\
 	$(BUILD_DIR)/asm/plugPikiNishimura/SpiderLeg.o\
 	$(BUILD_DIR)/asm/text_Nishimura.o\
+	$(BUILD_DIR)/asm/plugPikiOgawa/ogMessage.o\
 	$(BUILD_DIR)/asm/plugPikiOgawa/ogMemChk.o\
 	$(BUILD_DIR)/asm/plugPikiOgawa/ogDiary.o\
 	$(BUILD_DIR)/asm/plugPikiOgawa/ogMenu.o\
