84193ca6be2d8bc4ae431648cdf8ae6080817323
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-11-27 19:41:01 -0500

    split and label King.s

--- obj_files.mk
+++ obj_files.mk
@@ -42,6 +42,7 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/plugPikiNishimura/SpiderAi.o\
 	$(BUILD_DIR)/asm/plugPikiNishimura/SpiderLeg.o\
 	$(BUILD_DIR)/asm/text_Nishimura.o\
+	$(BUILD_DIR)/asm/plugPikiNishimura/King.o\
 	$(BUILD_DIR)/asm/plugPikiNishimura/KingAi.o\
 	$(BUILD_DIR)/asm/plugPikiNishimura/KingBody.o\
 	$(BUILD_DIR)/asm/plugPikiNishimura/Kogane.o\
