5bf763d2fb3967883e3e62b0eb3fff5d3e854ba0
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-12-05 15:21:00 -0500

    Upgrade frank to account for conditional branches
    frank will now handle conditional branches with care

--- tools/frank.py
+++ tools/frank.py
@@ -8,6 +8,13 @@ import argparse
 
 # Byte sequences that mark end of code
 EOC_BYTES = [b"\x4E\x80\x00\x20\x02", b"\x48\x00\x00\x01\x02"]
+CBLR_BYTES = [
+b"\x4D\x80\x00\x20", b"\x4D\x80\x00\x21", b"\x4C\x81\x00\x20", b"\x4C\x81\x00\x21",
+b"\x4D\x82\x00\x20", b"\x4D\x82\x00\x21", b"\x4C\x80\x00\x20", b"\x4C\x80\x00\x21",
+b"\x4D\x81\x00\x20", b"\x4D\x81\x00\x21", b"\x4C\x80\x00\x20", b"\x4C\x80\x00\x21",
+b"\x4C\x82\x00\x20", b"\x4C\x82\x00\x21", b"\x4C\x81\x00\x20", b"\x4C\x81\x00\x21",
+b"\x4D\x83\x00\x20", b"\x4D\x83\x00\x21", b"\x4C\x83\x00\x20", b"\x4C\x83\x00\x21",
+b"\x4D\x83\x00\x20", b"\x4D\x83\x00\x21", b"\x4C\x83\x00\x20", b"\x4C\x83\x00\x21"]
 
 # Example invocation: ./frank.py vanilla.o profile.o output.o
 parser = argparse.ArgumentParser()
@@ -45,6 +52,17 @@ assert(vanilla_eoc != len(vanilla_bytes))
 
 # Replace 0x34 - eoc in vanilla with bytes from stripped
 final_bytes = vanilla_bytes[:0x34] + stripped_bytes[0x34:stripped_eoc] + vanilla_bytes[stripped_eoc:]
+
+# Fix conditional branches
+for seq in CBLR_BYTES:
+    vanilla_idx = vanilla_bytes.count(seq)
+    final_idx = final_bytes.count(seq)
+    if vanilla_idx > final_idx:
+        vanilla_findpos = 0
+        for x in range(vanilla_idx):
+            vanilla_findpos = vanilla_bytes.find(seq, vanilla_findpos)
+            final_bytes = final_bytes[:vanilla_findpos] + vanilla_bytes[vanilla_findpos:vanilla_findpos+4] + final_bytes[vanilla_findpos+4:]
+            vanilla_findpos+=1
 
 with open(args.target, "wb") as f:
     f.write(final_bytes)
