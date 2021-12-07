ae794fc1cd830e3dc660de9ca53cb5be65cfcf5d
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-12-07 16:46:00 -0500

    split and label taiotomi.s

--- obj_files.mk
+++ obj_files.mk
@@ -38,6 +38,7 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/sysCommon/atx.o\
 	$(BUILD_DIR)/asm/sysCommon/id32.o\
 	$(BUILD_DIR)/asm/text_80044194.o\
+	$(BUILD_DIR)/asm/plugPikiNakata/taiotimoti.o\
 	$(BUILD_DIR)/asm/plugPikiNakata/taipalm.o\
 	$(BUILD_DIR)/asm/plugPikiNakata/taireactionactions.o\
 	$(BUILD_DIR)/asm/plugPikiNakata/taiswallow.o\
