4ea9b9b258ac49d01ea9802608be1db537cf3a95
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-12-09 14:57:13 -0500

    split and label taichappy.s

--- obj_files.mk
+++ obj_files.mk
@@ -38,6 +38,7 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/sysCommon/atx.o\
 	$(BUILD_DIR)/asm/sysCommon/id32.o\
 	$(BUILD_DIR)/asm/text_80044194.o\
+	$(BUILD_DIR)/asm/plugPikiNakata/taichappy.o\
 	$(BUILD_DIR)/asm/plugPikiNakata/taicollec.o\
 	$(BUILD_DIR)/asm/plugPikiNakata/taicollisionactions.o\
 	$(BUILD_DIR)/asm/plugPikiNakata/taieffectactions.o\
