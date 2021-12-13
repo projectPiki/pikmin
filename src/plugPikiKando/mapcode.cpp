f534db232c6ca3408c7183dd0c92a91aeeac7eb0
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-12-12 20:52:18 -0500

    match mapcode.cpp
    thanks to @intns for the ternary
    
    Co-Authored-By: intns <84647527+intns@users.noreply.github.com>

--- src/plugPikiKando/mapcode.cpp
+++ src/plugPikiKando/mapcode.cpp
@@ -1,95 +1,76 @@
 #include "types.h"
 
+extern "C" float sinf(float);
 
+struct CollTriInfo{
+    char filler[24];
+    f32 float1;
+    f32 float2;
+    f32 float3;
+    char filler2[4];
+    f32 float4;
+    f32 float5;
+    f32 float6;
+    f32 float7;
+};
+
+class MapCode {
+    u32 _00;
+    u32 _04;
+    u32 _08;
+    u32 _0C;
+    u32 _10;
+    u32 _14;
+    u32 _18;
+    f32 _1C;
+
+    u32 getAttribute(CollTriInfo*);
+    char* getAttributeName(int); // UNUSED
+    u32 getSlipCode(CollTriInfo *);
+    bool isBald(CollTriInfo *);
+};
 
 /*
  * --INFO--
  * Address:	80116080
  * Size:	00000C
  */
-void MapCode::getAttribute(CollTriInfo *)
+u32 MapCode::getAttribute(CollTriInfo*)
 {
-/*
-.loc_0x0:
-  lwz       r0, 0x0(r3)
-  rlwinm    r3,r0,3,29,31
-  blr
-*/
-}
+    return _00 >> 29;
+};
+
 
 /*
  * --INFO--
  * Address:	........
  * Size:	000018
  */
-void MapCode::getAttributeName(int)
+char* MapCode::getAttributeName(int)
 {
 	// UNUSED FUNCTION
-}
+	return "UNUSED";
+};
 
 /*
  * --INFO--
  * Address:	8011608C
  * Size:	00001C
  */
-void MapCode::getSlipCode(CollTriInfo *)
+u32 MapCode::getSlipCode(CollTriInfo *)
 {
-/*
-.loc_0x0:
-  cmplwi    r3, 0
-  beq-      .loc_0x14
-  lwz       r0, 0x0(r3)
-  rlwinm    r3,r0,5,30,31
-  blr       
-
-.loc_0x14:
-  li        r3, 0
-  blr
-*/
-}
+  if (this != (nullptr)) {
+    return _00 >> 27 & 3;
+  }
+  else { return 0; }
+};
 
 /*
  * --INFO--
  * Address:	801160A8
  * Size:	000068
  */
-void MapCode::isBald(CollTriInfo *)
+bool MapCode::isBald(CollTriInfo *)
 {
-/*
-.loc_0x0:
-  mflr      r0
-  stw       r0, 0x4(r1)
-  stwu      r1, -0x18(r1)
-  stw       r31, 0x14(r1)
-  mr.       r31, r3
-  beq-      .loc_0x50
-  lfs       f1, -0x6080(r2)
-  bl        0x105C24
-  lfs       f0, 0x1C(r31)
-  fcmpo     cr0, f0, f1
-  bge-      .loc_0x34
-  li        r3, 0x1
-  b         .loc_0x54
-
-.loc_0x34:
-  lwz       r0, 0x0(r31)
-  rlwinm.   r0,r0,7,31,31
-  beq-      .loc_0x48
-  li        r3, 0
-  b         .loc_0x54
-
-.loc_0x48:
-  li        r3, 0x1
-  b         .loc_0x54
-
-.loc_0x50:
-  li        r3, 0x1
-
-.loc_0x54:
-  lwz       r0, 0x1C(r1)
-  lwz       r31, 0x14(r1)
-  addi      r1, r1, 0x18
-  mtlr      r0
-  blr
-*/
+return (this != nullptr) ? ( _1C < sinf(0.7853982)) ? true : ((_00 >> 25 & 1) != 0) ? false : true : true;
 }
