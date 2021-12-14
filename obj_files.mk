71cf6566011076bbd39d8d34fb5afa9a836abb9b
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-12-13 21:58:38 -0500

    split and label naviDemoState.s

--- obj_files.mk
+++ obj_files.mk
@@ -38,6 +38,7 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/sysCommon/atx.o\
 	$(BUILD_DIR)/asm/sysCommon/id32.o\
 	$(BUILD_DIR)/asm/text_80044194.o\
+	$(BUILD_DIR)/asm/plugPikiKando/naviDemoState.o\
 	$(BUILD_DIR)/asm/plugPikiKando/gameCoreSection.o\
 	$(BUILD_DIR)/asm/plugPikiKando/gmWin.o\
 	$(BUILD_DIR)/asm/plugPikiKando/gameStat.o\
