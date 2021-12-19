65bbd2ff3148c933c56e5d416e2ff05e3219dbb0
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-12-19 16:32:19 -0500

    split and label keyItem.s

--- obj_files.mk
+++ obj_files.mk
@@ -38,6 +38,7 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/sysCommon/atx.o\
 	$(BUILD_DIR)/asm/sysCommon/id32.o\
 	$(BUILD_DIR)/asm/text_80044194.o\
+	$(BUILD_DIR)/asm/plugPikiKando/keyItem.o\
 	$(BUILD_DIR)/asm/plugPikiKando/ropeItem.o\
 	$(BUILD_DIR)/asm/plugPikiKando/seedItem.o\
 	$(BUILD_DIR)/asm/plugPikiKando/genItem.o\
