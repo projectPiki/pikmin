8f8f0784d34f53a6bcbef760f1b66e24756e571f
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-12-29 14:03:53 -0500

    split and label aiKinoko.s

--- obj_files.mk
+++ obj_files.mk
@@ -37,6 +37,7 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/sysCommon/atx.o\
 	$(BUILD_DIR)/asm/sysCommon/id32.o\
 	$(BUILD_DIR)/asm/text_80044194.o\
+	$(BUILD_DIR)/asm/plugPikiKando/aiKinoko.o\
 	$(BUILD_DIR)/asm/plugPikiKando/aiChase.o\
 	$(BUILD_DIR)/asm/plugPikiKando/aiCrowd.o\
 	$(BUILD_DIR)/asm/plugPikiKando/aiDecoy.o\
