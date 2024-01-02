dd5a014354756a52d5db3f7db4edb62a97460c77
intns <84647527+intns@users.noreply.github.com>
2024-01-02 18:29:12 +0000

    Update system.h

--- include/system.h
+++ include/system.h
@@ -45,56 +45,56 @@ struct StdSystem {
 
 	static char* stringDup(char*);
 
-	bool m_pending;                 // _04
-	f32 m_currentFade;              // _08
-	f32 m_fadeStart;                // _0C
-	f32 m_fadeEnd;                  // _10
-	struct Font* m_consFont;        // _14
-	s32 m_frameClamp;               // _18
-	TimerState m_timerState;        // _1C
-	u32 m_togglePrint;              // _20
-	u32 m_toggleDInfo;              // _24
-	u32 m_toggleDExtra;             // _28
-	u32 m_toggleBlur;               // _2C
-	u32 m_toggleFileInfo;           // _30
-	u32 m_toggleColls;              // _34
-	struct Timers* m_timer;         // _38
-	struct TextureCacher* m_cacher; // _3C
-	u32 m_dword40;                  // _40
-	Matrix4f* m_matrix;             // _44
-	s8* m_bloDirectory;             // _48
-	s8* m_texDirectory;             // _4C
-	s8* m_currentDirectory;         // _50
-	s8* m_dataRoot;                 // _54
-	AyuHeap m_heaps[8];             // _58 (sys, ovl, app, load, teki, movie, message, lang)
-	s32 m_activeHeapNum;            // _5C, TODO: the offsets must be wrong, LOL
-	u32 m_dword19C;                 // _19C
-	u32 m_dword1A0;                 // _1A0
-	u32 m_dword1A4;                 // _1A4
-	u32 m_materialCount;            // _1A8
-	u32 m_dispCount;                // _1AC
-	u32 m_lightCount;               // _1B0
-	u32 m_dword1B4;                 // _1B4
-	u32 m_loadedPolys;              // _1B8
-	u32 m_lightingSkips;            // _1BC
-	u32 m_lightingSets;             // _1C0
-	u32 m_systemState;              // _1C4
-	u32 m_systemFlags;              // _1C8
-	Graphics* m_graphics;           // _1CC
-	GfxobjInfo m_gfxObjects;        // _1D0
-	bool m_hasGfxObjects;           // _1F0
-	char* m_textureBase1;           // _1F4
-	char* m_textureBase2;           // _1F8
-	BaseShape* m_currentShape;      // _1FC
-	CoreNode m_coreNode200;         // _200
-	CoreNode m_coreNode214;         // _214
-	u32 m_dword228;                 // _228
-	u32 m_flareCount;               // _22C
-	u32 m_lfInfoCount;              // _230
-	LFInfo* m_lfInfo;               // _234
-	LightFlare* m_lightFlares;      // _238
-	LFlareGroup* m_lfGroups;        // _23C
-	u32 m_dword240;                 // _240
+	bool mPending;                 // _04
+	f32 mCurrentFade;              // _08
+	f32 mFadeStart;                // _0C
+	f32 mFadeEnd;                  // _10
+	struct Font* mConsFont;        // _14
+	s32 mFrameRate;                // _18
+	TimerState mTimerState;        // _1C
+	u32 mTogglePrint;              // _20
+	u32 mToggleDebugInfo;          // _24
+	u32 mToggleDebugExtra;         // _28
+	u32 mToggleBlur;               // _2C
+	u32 mToggleFileInfo;           // _30
+	u32 mToggleColls;              // _34
+	struct Timers* mTimer;         // _38
+	struct TextureCacher* mCacher; // _3C
+	u32 _40;                       // _40
+	Matrix4f* mMatrix;             // _44
+	s8* mBloDirectory;             // _48
+	s8* mTexDirectory;             // _4C
+	s8* mCurrentDirectory;         // _50
+	s8* mDataRoot;                 // _54
+	AyuHeap mHeaps[8];             // _58 (sys, ovl, app, load, teki, movie, message, lang)
+	s32 mActiveHeapIdx;            // _198
+	u32 _19C;                      // _19C
+	u32 _1A0;                      // _1A0
+	u32 _1A4;                      // _1A4
+	u32 mMaterialCount;            // _1A8
+	u32 mDispCount;                // _1AC
+	u32 mLightCount;               // _1B0
+	u32 _1B4;                      // _1B4
+	u32 mLoadedPolys;              // _1B8
+	u32 mLightingSkips;            // _1BC
+	u32 mLightingSets;             // _1C0
+	u32 mSystemState;              // _1C4
+	u32 mSystemFlags;              // _1C8
+	Graphics* mGraphics;           // _1CC
+	GfxobjInfo mGfxObjects;        // _1D0
+	bool mHasGfxObjects;           // _1F0
+	char* mTextureBase1;           // _1F4
+	char* mTextureBase2;           // _1F8
+	BaseShape* mCurrentShape;      // _1FC
+	CoreNode _200;                 // _200
+	CoreNode _214;                 // _214
+	u32 _228;                      // _228
+	u32 mFlareCount;               // _22C
+	u32 mLfInfoCount;              // _230
+	LFInfo* mLfInfo;               // _234
+	LightFlare* mLightFlares;      // _238
+	LFlareGroup* mLfGroups;        // _23C
+	u32 _240;                      // _240
 };
 
 struct System : public StdSystem {
@@ -115,11 +115,11 @@ struct System : public StdSystem {
 	static void* alloc(size_t);
 
 	// _244
-	u32 _248;
+	u32 _248;        // _248
 	Graphics* mGfx;  // _24C
 	char _250[0x3C]; // _250
-	f32 _28C;
-	char _290[0xA4];
+	f32 _28C;        // _28C
+	char _290[0xA4]; // _290
 };
 
 extern System* gsys;
@@ -128,6 +128,7 @@ struct NodeMgr {
 	char filler0[0x18];
 	NodeMgr();
 };
+
 extern NodeMgr* nodeMgr;
 
 extern "C" void OSPanic(const char* filename, int line, const char* msg, ...);
