05126a349e8efbe0f3d43f40793fabacb5802dad
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2025-03-16 01:56:45 -0500

    types.h: add int<x>_t typedefs

--- include/types.h
+++ include/types.h
@@ -48,6 +48,10 @@ typedef u16 wchar_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
+/* int<x>_t */
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
 
 typedef int intptr_t;
 typedef unsigned int uintptr_t;
