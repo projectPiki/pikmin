8702ca90b93dce7f319a3489c6092c181ffdf735
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2025-03-21 18:22:22 -0500

    JAudio: Header scaffolding

---
+++ include/jaudio/virload.h
@@ -0,0 +1,18 @@
+#ifndef _JAUDIO_VIRLOAD_H
+#define _JAUDIO_VIRLOAD_H
+
+#include "types.h"
+
+void JV_InitHeader(char*);
+void JV_InitHeader_M(char*, u8*, u8*);
+void JV_GetArchiveHandle(char*);
+void JV_GetLogicalHandleS(char*, char*);
+void JV_GetLogicalHandle(char*);
+void JV_GetHandle(u32);
+void JV_GetRealHandle(u32);
+void JV_CheckSize(u32);
+void JV_LoadFile(u32, u8*, u32, u32);
+void JV_LoadFile_Async2(u32, u8*, u32, u32, void (*)(u32), u32);
+void JV_GetMemoryFile(u32);
+
+#endif
