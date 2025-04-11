6f01ee3a6e86e000470eaf17877583d7103f6b44
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2025-04-10 21:52:40 -0500

    virload.c: Old news
    I'm so bad about hoarding progress...

--- include/jaudio/virload.h
+++ include/jaudio/virload.h
@@ -1,6 +1,8 @@
 #ifndef _JAUDIO_VIRLOAD_H
 #define _JAUDIO_VIRLOAD_H
 
+#include "jaudio/pikiseq.h"
+
 #include "types.h"
 
 void JV_InitHeader(char*);
@@ -9,9 +11,9 @@ u32 JV_GetArchiveHandle(char*);
 void JV_GetLogicalHandleS(char*, char*);
 void JV_GetLogicalHandle(char*);
 void JV_GetHandle(u32);
-void JV_GetRealHandle(u32);
+BarcEntry* JV_GetRealHandle(u32 handle); // Return type unsure
 u32 JV_CheckSize(u32);
-unknown JV_LoadFile(u32, u8*, u32, u32);
+u32 JV_LoadFile(u32, u8*, u32, u32);
 unknown JV_LoadFile_Async2(u32, u8*, u32, u32, void (*)(u32), u32);
 void JV_GetMemoryFile(u32);
 
