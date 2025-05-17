0500b388106781a5833e9cdb9122ebdca9e200ad
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2025-05-17 02:00:50 -0500

    bankloader.c: Matching

--- src/jaudio/bankloader.c
+++ src/jaudio/bankloader.c
@@ -5,6 +5,31 @@
 #include "jaudio/aictrl.h"
 #include "jaudio/waveread.h"
 
+typedef struct BXHeader BXHeader;
+typedef struct AddrSize AddrSize;
+
+/**
+ * @brief This is an invented type of an unknown name. It represents a file structure.
+ *
+ * @note Size: 0x10. Based on https://github.com/XAYRGA/JaiSeqX/blob/sxlja-newenv/JaiSeqXLJA/libJAudio/Loaders/JA_BXLoader.cs
+ */
+struct BXHeader {
+	u32 wsysPointerOffset;
+	u32 wsysPointerCount;
+	u32 ibnkPointerOffset;
+	u32 ibnkPointerCount;
+};
+
+/**
+ * @brief This is an invented type of an unknown name. It represents a file structure.
+ *
+ * @note Size: 0x8.
+ */
+struct AddrSize {
+	u32 addr;
+	u32 size;
+};
+
 /*
  * --INFO--
  * Address:	8001D9A0
@@ -12,42 +37,36 @@
  */
 void Bank_Setup(char* filename)
 {
-	u32 iVar1;
-	u32 iVar2;
-	u32 iVar3;
-
 	u32 i;
-	u8* space;
+
+	u8* mem;
+	BXHeader* header;
+	AddrSize* pairs;
+	u32 pointerCount;
 
 	Bank_Init();
 
-	space = (u8*)OSAlloc2(DVDT_CheckFile(filename));
-	DVDT_LoadFile(filename, space);
+	mem = (u8*)OSAlloc2(DVDT_CheckFile(filename));
+	DVDT_LoadFile(filename, mem);
+	header = (BXHeader*)mem;
 
 	Wavegroup_Init();
 
-	// Nothing past this point is particularly good. The space pointer
-	// is probably cast to some kind of struct that represents the bank.
-	// Super Mario Sunshine's `TBasicWaveBank` seems promising.
-	// FYI: This function is used to load pikibank.bx
+	pointerCount = header->wsysPointerCount;
+	pairs        = (AddrSize*)(mem + header->wsysPointerOffset);
 
-	iVar1 = *(u32*)(space + 0);
-	iVar2 = *(u32*)(space + 4);
-	iVar3 = 0;
-	for (i = 0; i < iVar2; ++i) {
-		if (*(u32*)(space + iVar3 + iVar1 + 4)) {
-			Wavegroup_Regist(space + *(u32*)(space + iVar3 + iVar1), i);
+	for (i = 0; i < pointerCount; ++i) {
+		if (pairs[i].size != 0) {
+			Wavegroup_Regist((void*)(mem + pairs[i].addr), i);
 		}
-		iVar3 += 8;
 	}
 
-	iVar1 = *(u32*)(space + 8);
-	iVar2 = *(u32*)(space + 12);
-	iVar3 = 0;
-	for (i = 0; i < iVar2; ++i) {
-		if (*(u32*)(space + iVar3 + iVar1 + 4)) {
-			Bank_Regist(space + *(u32*)(space + iVar3 + iVar1), i);
+	pointerCount = header->ibnkPointerCount;
+	pairs        = (AddrSize*)(mem + header->ibnkPointerOffset);
+
+	for (i = 0; i < pointerCount; ++i) {
+		if (pairs[i].size != 0) {
+			Bank_Regist((void*)(mem + pairs[i].addr), i);
 		}
-		iVar3 += 8;
 	}
 }
