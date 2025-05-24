f51d61bae85edfe7861a0fce67c80a620449e56c
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2025-05-24 01:05:36 -0500

    seqsetup.c: WIP
    Moar `seqp_` info. `Init_Track` seems promising.

--- include/jaudio/seqsetup.h
+++ include/jaudio/seqsetup.h
@@ -11,12 +11,12 @@ typedef struct seqp_ seqp_;
 
 void Jaq_Reset(void);
 void Jaq_GetRemainFreeTracks(void);
-unknown Jaq_HandleToSeq(u32 handle);
+seqp_* Jaq_HandleToSeq(u32 handle);
 BOOL Jaq_StopSeq(u32 index);
 unknown Jaq_SetSeqData(unknown, u8*, u32, unknown);
 unknown Jaq_SetSeqData_Limit(unknown, u8*, u32, unknown, unknown);
 void Jaq_SetBankNumber(int*, u8);
-void Jaq_StartSeq(u32);
+BOOL Jaq_StartSeq(u32);
 void Jaq_OpenTrack(seqp_*, unknown, unknown);
 void Jaq_CloseTrack(seqp_*);
 
