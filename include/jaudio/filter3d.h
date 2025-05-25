2ae090925eb42c8969d5ef949663284dfa653c66
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2025-05-25 00:02:41 -0500

    JAudio: Finish scaffolding headers
    And rename a few that don't match their source files' names.

--- include/jaudio/filter3d.h
+++ include/jaudio/filter3d.h
@@ -1,7 +1,8 @@
-#ifndef _V3D_H
-#define _V3D_H
+#ifndef _JAUDIO_FILTER3D_H
+#define _JAUDIO_FILTER3D_H
 
 #include "types.h"
+
 typedef struct Vector3D_ Vector3D_;
 typedef struct TVector3D_ TVector3D_;
 typedef struct Object3D_ Object3D_;
@@ -11,7 +12,7 @@ struct Vector3D_ {
 	f32 x, y, z;
 };
 
-f64 atanf2(f32, f32);
-f64 sqrtf2(f32);
+f32 V3D_GetAngle(Vector3D_* input_vec);
+f64 V3D_Abs(Vector3D_* input_vec);
 
 #endif
