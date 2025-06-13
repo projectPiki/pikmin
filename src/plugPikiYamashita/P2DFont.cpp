d07342a94bade2685f14cee02d9db47c19c90495
cadmic <cadmic24@gmail.com>
2025-06-12 22:57:00 -0700

    Match P2DFont::getWidth

--- src/plugPikiYamashita/P2DFont.cpp
+++ src/plugPikiYamashita/P2DFont.cpp
@@ -179,63 +179,8 @@ f32 P2DFont::getWidth(int charCode, int drawWidth)
 {
 	f32 a = mFont->mChars[charToIndex(charCode)].mCharSpacing;
 	f32 b = drawWidth;
-	return b / getWidth() * a + 1.0f;
-	/*
-	.loc_0x0:
-	  mflr      r0
-	  stw       r0, 0x4(r1)
-	  rlwinm.   r0,r4,0,0,23
-	  stwu      r1, -0x40(r1)
-	  stw       r31, 0x3C(r1)
-	  addi      r31, r5, 0
-	  stw       r30, 0x38(r1)
-	  addi      r30, r3, 0
-	  beq-      .loc_0x34
-	  lwz       r3, 0x0(r30)
-	  rlwinm    r4,r4,0,16,31
-	  bl        -0x18DF40
-	  b         .loc_0x40
-
-	.loc_0x34:
-	  lwz       r3, 0x0(r30)
-	  rlwinm    r4,r4,0,24,31
-	  bl        -0x18DFF4
-
-	.loc_0x40:
-	  lhz       r0, 0x6(r30)
-	  xoris     r4, r31, 0x8000
-	  lwz       r6, 0x0(r30)
-	  lis       r5, 0x4330
-	  stw       r4, 0x2C(r1)
-	  lfd       f3, -0x48E8(r2)
-	  mulli     r3, r3, 0x1C
-	  stw       r0, 0x24(r1)
-	  lwz       r0, 0xC(r6)
-	  stw       r5, 0x28(r1)
-	  lfd       f1, -0x48D8(r2)
-	  add       r3, r0, r3
-	  stw       r5, 0x20(r1)
-	  lfd       f2, 0x28(r1)
-	  lfd       f0, 0x20(r1)
-	  fsubs     f2, f2, f3
-	  lha       r0, 0x8(r3)
-	  fsubs     f1, f0, f1
-	  lfs       f0, -0x48E0(r2)
-	  xoris     r0, r0, 0x8000
-	  fdivs     f1, f2, f1
-	  stw       r0, 0x34(r1)
-	  stw       r5, 0x30(r1)
-	  lfd       f2, 0x30(r1)
-	  fsubs     f2, f2, f3
-	  fmuls     f2, f2, f1
-	  fadds     f1, f0, f2
-	  lwz       r0, 0x44(r1)
-	  lwz       r31, 0x3C(r1)
-	  lwz       r30, 0x38(r1)
-	  addi      r1, r1, 0x40
-	  mtlr      r0
-	  blr
-	*/
+	a *= b / getWidth();
+	return a + 1.0f;
 }
 
 /*
