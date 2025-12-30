be6bddf926f6215511654a1943b5aab8fd281966
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2025-12-30 00:53:43 -0600

    Nitpicks

--- src/plugPikiKando/gameCoreSection.cpp
+++ src/plugPikiKando/gameCoreSection.cpp
@@ -1824,7 +1824,7 @@ void GameCoreSection::draw2D(Graphics& gfx)
 		seSystem->draw2d(gfx);
 	}
 
-	if (AIPerf::moveType) {
+	if (AIPerf::moveType != 0) {
 		gfx.useTexture(mMapMgr->mBlurredTexture, GX_TEXMAP0);
 		GXSetTevSwapModeTable(GX_TEV_SWAP0, GX_CH_RED, GX_CH_GREEN, GX_CH_BLUE, GX_CH_ALPHA);
 		GXSetTevSwapModeTable(GX_TEV_SWAP1, GX_CH_RED, GX_CH_RED, GX_CH_RED, GX_CH_ALPHA);
