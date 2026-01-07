cea44933761c2d945aad40fcca8d8a16146c471c
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2026-01-07 08:33:04 -0600

    Document `Timers::showTimes`

--- src/sysCommon/timers.cpp
+++ src/sysCommon/timers.cpp
@@ -103,6 +103,9 @@ void Timers::showTimes()
 	}
 }
 
+// BUG: This assumes the game is running at 60fps, when (most of the time) it is not.
+#define MILLISEC_PER_FRAME (1000.0f / 60.0f)
+
 /**
  * @todo: Documentation
  */
@@ -111,109 +114,123 @@ void Timers::draw(Graphics& gfx, Font* font)
 #if defined(DEVELOP) || defined(VERSION_G98E01_PIKIDEMO)
 	gfx.useTexture(nullptr, GX_TEXMAP0);
 
-	// I'm leaving the variables with their Ghidra names for now because they were helpful for navigating the function's stack.  This
-	// should be all of the variables in the exact order they were defined, which Ghidra bungles and I had to read assembly to learn.
-
-	for (TimerInf* local_c = mTimerList; local_c; local_c = local_c->mNext) {
-		local_c->mSampleTime++;
-		if (local_c->mSampleTime == 10) {
-			local_c->mAverageTime = local_c->mDuration / 10.0f;
-			if (local_c->mAverageTime < 0.0f) {
-				local_c->mAverageTime = 0.0f;
+	// Calculate the peak and average times of the various subsystems now.
+	for (TimerInf* info = mTimerList; info; info = info->mNext) {
+		// For the average time, we measure 10 frames worth of data.
+		info->mSampleTime++;
+		if (info->mSampleTime == 10) {
+			info->mAverageTime = info->mDuration / 10.0f;
+			// This function even handles time-travelling!  Wow!
+			if (info->mAverageTime < 0.0f) {
+				info->mAverageTime = 0.0f;
 			}
-			if (local_c->mAverageTime > local_c->mPeakTime) {
-				local_c->mDecayTime = 10.0f;
-				local_c->mPeakTime  = local_c->mAverageTime;
+			// Establish a new peak time if the average time exceeds it.
+			if (info->mAverageTime > info->mPeakTime) {
+				// And hold that mark of shame on the chart for 10 seconds.
+				info->mDecayTime = 10.0f;
+				info->mPeakTime  = info->mAverageTime;
 			}
-			local_c->mDuration   = 0.0f;
-			local_c->mSampleTime = 0;
+			info->mDuration   = 0.0f;
+			info->mSampleTime = 0;
 		}
 
-		local_c->mDecayTime -= gsys->getFrameTime();
-		if (local_c->mDecayTime < 0.0f && local_c->mPeakTime > 0.0f) {
-			local_c->mPeakTime -= 6.0f * gsys->getFrameTime();
-			if (local_c->mPeakTime < 0.0f) {
-				local_c->mPeakTime = 0.0f;
+		info->mDecayTime -= gsys->getFrameTime();
+		// If enough time has passed since the last lag-spike, start wiping it away.
+		if (info->mDecayTime < 0.0f && info->mPeakTime > 0.0f) {
+			info->mPeakTime -= 6.0f * gsys->getFrameTime();
+			if (info->mPeakTime < 0.0f) {
+				info->mPeakTime = 0.0f;
 			}
 		}
 	}
 
-	// Making any of the following named constants `const` breaks the stack for matching.  Lovely `const` memes!
+	// Making these three named constants `const` breaks the stack for matching.  Lovely `const` memes!
+	immut f32 chartMarginSize = TERNARY_BUGFIX(256.0f, 192.0f);
+	immut int chartTimeScale  = 4; // How many multiples of `MILLISEC_PER_FRAME` to fit on the chart
+	immut int chartBarHeight  = 12;
 
-	immut f32 local_10 = TERNARY_BUGFIX(256.0f, 192.0f);         // Chart Horizontal Margin Size (Combined)
-	immut int local_14 = 4;                                      // Chart Horizontal Scale
-	immut int local_18 = 12;                                     // Chart Vertical Scale
-	                                                             // ------------------------------
-	immut f32 local_1c = gfx.mScreenWidth - local_10;            // Chart Width
-	immut f32 local_20 = local_1c / local_14;                    // Chart Tick Width
-	immut f32 local_24 = local_1c;                               // unused
-	immut f32 local_28 = (f32)mTimerCount * local_18 + 32.0f;    // Chart Height
-	immut f32 local_2c = gfx.mScreenWidth / 2 - local_1c / 2.0f; // Drawing Origin X
-	immut f32 local_30 = gfx.mScreenHeight - local_28;           // Drawing Origin Y
+	// Further calculations on how to draw the timers chart.
+	const f32 chartWidth     = gfx.mScreenWidth - chartMarginSize;
+	const f32 chartTickWidth = chartWidth / chartTimeScale;
+	const f32 local_24       = chartWidth; // unused
+	const f32 chartHeight    = (f32)mTimerCount * chartBarHeight + 32.0f;
+	const f32 local_2c       = gfx.mScreenWidth / 2 - chartWidth / 2.0f; // upper-left origin of the chart
+	const f32 chartOriginY   = gfx.mScreenHeight - chartHeight;          // upper-left origin of the chart
 
-	// These really didn't need to exist, but they do according to the DLL.
-	immut f32 local_34 = local_2c; // Replaces `local_2c` in usage
-	immut f32 local_38 = local_30; // unused
+	// These really didn't need to exist, but they do according to the DLL, and `chartOriginX` is load-bearing.
+	const f32 chartOriginX = local_2c;     // Replaces `local_2c` in usage
+	const f32 local_38     = chartOriginY; // Unused, because of course nothing is ever consistent.
 
-	f32 local_3c;
-	f32 local_40 = local_30;
-	int local_44 = 0;
+	// First we draw the checkered background of the timer chart, indicating the time scale chosen by `chartTimeScale`.
+	f32 drawPosX;
+	f32 drawPosY = chartOriginY;
 
-	for (TimerInf* local_48 = mTimerList; local_48; local_48 = local_48->mNext) {
-		local_3c = local_34;
-		for (int local_4c = 0; local_4c < local_14; ++local_4c) {
-			// Something horrendous is going here.  Based on my understanding of stack variables in the DLL, there are no more
-			// lvalues in this function, therefore this must be a ternary.  Just what is going on with this condition, though?
-			int local_50 = (((local_44 & 1) + local_4c & 1) ? 0.1f : 0.3f) * 255.0f;
+	int chartCurrBar = 0;
+	for (TimerInf* info = mTimerList; info; info = info->mNext) {
+		drawPosX = chartOriginX;
+		for (int chartCurrTick = 0; chartCurrTick < chartTimeScale; ++chartCurrTick) {
+			// Alternate between the shades #191919 and #4C4C4C for the checkered background
+			int value = (((chartCurrBar & 1) + chartCurrTick & 1) ? 0.1f : 0.3f) * 255.0f;
 
-			gfx.setColour(Colour(local_50, local_50, local_50, 128), true);
-			gfx.setAuxColour(Colour(local_50, local_50, local_50, 128));
+			gfx.setColour(Colour(value, value, value, 128), true);
+			gfx.setAuxColour(Colour(value, value, value, 128));
 
-			gfx.fillRectangle(RectArea(local_3c, local_40, local_3c + local_20, local_40 + local_18));
-			local_3c += local_20;
+			gfx.fillRectangle(RectArea(drawPosX, drawPosY, drawPosX + chartTickWidth, drawPosY + chartBarHeight));
+			drawPosX += chartTickWidth;
 		}
-		local_40 += local_18;
-		++local_44;
+		drawPosY += chartBarHeight; // Reposition for the next bar.
+		++chartCurrBar;
 	}
 
-	local_3c = local_34;
-	local_40 = local_30 + 2.0f;
+	// Then we draw the actual bar graphs: green or red for average time, orange for peak time).
+	drawPosX = chartOriginX;
+	drawPosY = chartOriginY + 2.0f;
 
-	for (TimerInf* local_54 = mTimerList; local_54; local_54 = local_54->mNext) {
-		f32 local_58 = local_54->mAverageTime / 16.666666f;
+	for (TimerInf* info = mTimerList; info; info = info->mNext) {
+		f32 chartBarAvgTimeScale = info->mAverageTime / MILLISEC_PER_FRAME;
 
-		if (local_58 > local_14) {
+		// If the bar we're about to draw is LITERALLY off the charts.
+		if (chartBarAvgTimeScale > chartTimeScale) {
+			// UNACCEPTABLE LAG SPIKE DETECTED!  YOU GET THE RED BAR OF SHAME!
 			gfx.setColour(Colour(255, 0, 0, 255), true);
 			gfx.setAuxColour(Colour(32, 0, 0, 255));
-			local_58 = local_14;
+			chartBarAvgTimeScale = chartTimeScale; // Don't actually go off the charts (yet).
 		} else {
+			// Otherwise, you get the nice green coloured bar.
 			gfx.setColour(Colour(128, 255, 128, 255), true);
 			gfx.setAuxColour(Colour(0, 32, 0, 255));
 		}
 
-		local_58 *= local_20;
-		gfx.fillRectangle(RectArea(local_3c, local_40, local_3c + local_58, int(local_40) + local_18 - 4));
+		// Convert from time scale to display scale.
+		chartBarAvgTimeScale *= chartTickWidth;
+		gfx.fillRectangle(RectArea(drawPosX, drawPosY, drawPosX + chartBarAvgTimeScale, int(drawPosY) + chartBarHeight - 4));
 
-		f32 local_5c = local_54->mPeakTime / 16.666666f;
-		local_5c *= local_20;
-		gfx.setColour(Colour(0xff, 0xc0, 0x40, 0x60), true);
-		gfx.setAuxColour(Colour(0x20, 0, 0, 0x60));
-		gfx.fillRectangle(RectArea(local_3c + local_58, local_40, local_3c + local_5c, int(local_40) + local_18 - 4));
+		// Draw the peak time bar in much the same way (though this one is allowed to go off the charts).
+		f32 chartBarPeakTimeScale = info->mPeakTime / MILLISEC_PER_FRAME;
+		chartBarPeakTimeScale *= chartTickWidth;
+		gfx.setColour(Colour(255, 192, 64, 96), true);
+		gfx.setAuxColour(Colour(32, 0, 0, 96));
+		gfx.fillRectangle(
+		    RectArea(drawPosX + chartBarAvgTimeScale, drawPosY, drawPosX + chartBarPeakTimeScale, int(drawPosY) + chartBarHeight - 4));
 
-		local_40 += local_18;
+		drawPosY += chartBarHeight; // Reposition for the next bar.
 	}
-	local_40 = local_30 - 1.0f;
 
-	char local_15c[PATH_MAX];
-	sprintf(local_15c, "%.1f fps", gsys->getFrameRate());
+	// Next we draw an FPS counter at the top of the chart.
+	drawPosY = chartOriginY - 1.0f;
+
+	char fpsStr[PATH_MAX];
+	sprintf(fpsStr, "%.1f fps", gsys->getFrameRate());
 	gfx.setColour(Colour(255, 255, 255, 255), true);
-	gfx.texturePrintf(font, gfx.mScreenWidth / 2 - font->stringWidth(local_15c) + 30, int(local_40) - 14, "%s", local_15c);
+	gfx.texturePrintf(font, gfx.mScreenWidth / 2 - font->stringWidth(fpsStr) + 30, int(drawPosY) - 14, "%s", fpsStr);
 
+	// Finally we annotate the chart with timer names and the precise time measured in fractions of a frame.
 	gfx.setColour(Colour(255, 255, 255, 128), true);
-	for (TimerInf* local_160 = mTimerList; local_160; local_160 = local_160->mNext) {
-		gfx.texturePrintf(font, int(local_10 * 0.5f) - font->stringWidth(local_160->mTimerName), local_40, "%s", local_160->mTimerName);
-		gfx.texturePrintf(font, gfx.mScreenWidth - int(local_10 * 0.5f), local_40, "%.4f", local_160->mAverageTime / 16.666666f);
-		local_40 += local_18;
+	for (TimerInf* info = mTimerList; info; info = info->mNext) {
+		// Timer name on the left, precise time (ratio) on the right.
+		gfx.texturePrintf(font, int(chartMarginSize * 0.5f) - font->stringWidth(info->mTimerName), drawPosY, "%s", info->mTimerName);
+		gfx.texturePrintf(font, gfx.mScreenWidth - int(chartMarginSize * 0.5f), drawPosY, "%.4f", info->mAverageTime / MILLISEC_PER_FRAME);
+		drawPosY += chartBarHeight;
 	}
 #endif
 }
