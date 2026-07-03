5d1cacc73dcdaf6c2cf8a2f6e09da6533e6b0c3d
Minty-Meeo <45425365+Minty-Meeo@users.noreply.github.com>
2026-07-03 02:25:36 -0500

    DLL Decomp: bugprint.cpp
    That's a long-time mystery solved, folks.

---
+++ include/bugprint.h
@@ -0,0 +1,35 @@
+#ifndef _BUGPRINT_H
+#define _BUGPRINT_H
+
+#include "types.h"
+
+/**
+ * @todo Documentation
+ */
+struct BugPrintBuffer {
+public:
+	BugPrintBuffer();
+
+	void clear(void);
+	void update(void);
+	void print(immut char*, int);
+
+	int getFrame(void) { return mFrame; }
+
+protected:
+	int findLine(int);
+
+	char* mBuffer; // _00
+	int mBufPos;   // _04
+	int mCapacity; // _08
+	int mFrame;    // _0C
+};
+
+extern BugPrintBuffer* bugPrintBuffer;
+
+// This symbol has no mangling in the ILK.  Maybe it was meant to be accessible to even C code?
+extern "C" {
+void _bugPrint(immut char* fmt, ...);
+}
+
+#endif
